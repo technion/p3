79fe36513dbf297c674858744d482c8beff27088
Technion <technion@lolware.net>
2018-08-18 12:32:01 +0000

    Move some CSS outside dynamic code.

--- frontend/src/p3-entry.tsx
+++ frontend/src/p3-entry.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom"; // Aliased to react-dom-lite
-import CssBaseline from "@material-ui/core/es/CssBaseline";
-import Typography from "@material-ui/core/es/Typography";
 
 import { P3Form } from "./p3-form";
 
@@ -9,10 +7,6 @@ class App extends React.Component<{}, {}> {
   public render() {
     return (
       <React.Fragment>
-        <CssBaseline />
-        <Typography variant="display4" gutterBottom align="center">
-          P3 AD Password Reset Utility
-        </Typography>
         <P3Form />
         <div id="g-recaptcha"></div>
       </React.Fragment>
